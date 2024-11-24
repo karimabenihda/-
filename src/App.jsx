6268ea32c0e7a28f2b2b66560c97314579929d57
karimabenihda@gmail.com <karimabenihda@gmail.com>
2024-11-24 15:29:06 +0100

    My first Commit ✿

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Fontfr from './Fontfr'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  
+
+  return (
+    <>
+      <Fontfr/>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure it matches your project structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
